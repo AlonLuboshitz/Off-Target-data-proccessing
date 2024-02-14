03925319316c6ffdc3c7a8ee3529120731bbdc54
Alon Luboshitz <117107259+AlonLuboshitz@users.noreply.github.com>
2024-02-14 17:01:22 +0200

    Create Server_constants.py

---
+++ Server_constants.py
@@ -0,0 +1,10 @@
+
+
+## PATHS:
+EPIGENETIC_FOLDER = "/home/dsi/lubosha/Off-Target-data-proccessing/Epigenetics"
+BED_FILES_FOLDER = "/home/dsi/lubosha/Off-Target-data-proccessing/Epigenetics/Chromstate"
+BIG_WIG_FOLDER = "/home/dsi/lubosha/Off-Target-data-proccessing/Epigenetics/bigwig"
+CHANGESEQ_CASO = "/home/dsi/lubosha/Off-Target-data-proccessing/merged_csgs_casofinder.csv"
+CHANGESEQ_CASO_EPI = "/home/dsi/lubosha/Off-Target-data-proccessing/merged_csgs_casofinder_withEpigenetic.csv"
+CHANGESEQ_GS = "/home/dsi/lubosha/Off-Target-data-proccessing/merged_csgs.csv"
+CHANGESEQ_GS_EPI = "/home/dsi/lubosha/Off-Target-data-proccessing/merged_csgs_withEpigenetic.csv"
