added epigenetic perturbation and replacing argmax function. need to fix shap for epigenetic and gradient asecnt

interpertation.py
@@ -238,8 +238,8 @@ def get_shaply_values(model, x_background, explainer_type, x_selected = None):
         #     x_background = [x[:100] for x in x_background if x.shape[0] > 100]
         # elif x_background.shape[0] > 100:
         #     x_background = x_background[:100]
-        explainer = shap.Explainer(model, x_background)
-        #explainer = shap.explainers.Permutation(model,x_background ,max_evals = 15000)
+        #explainer = shap.Explainer(model, x_background)
+        explainer = shap.explainers.Permutation(model,x_background ,max_evals = 15000)
         #explainer = shap.DeepExplainer(model, x_background)
     elif explainer_type == 'gradient':
         explainer = shap.GradientExplainer(model, x_background)
@@ -276,7 +276,7 @@ def transform_to_heatmap(shap_values, seqeunce_length, bits_per_base, additional
 
 def run_shap(model_path,data_path,explainer_type,output_path,
              num_of_points=None,specific_indices=None, specific_guides=None,
-             only_seq=False):
+             only_seq=False, plot_all_guides = True, plot_single_guides = True):
     '''
     Runs on the data and model given and extract shap values
     Plots the bars, beeswarm and waterfall plots
@@ -291,22 +291,36 @@ def run_shap(model_path,data_path,explainer_type,output_path,
         specific_guides (list, optional): Specific guides to extract from x_background.
         only_seq (bool, optional) defualt True: If True, only the sequence features will be used otherwise split to sequence and epigenetics.
     '''
+    if not (plot_all_guides or plot_single_guides):
+        raise ValueError("At least one of the plot options should be True")
     models = get_model(model_path,"deep")
     model = models[0]
     x_background,y,guides,otss_dict = get_data(data_path,only_seq)
     if specific_guides is None:
         specific_guides = guides
     guide_idx = keep_intersect_guides_indices(guides,specific_guides)
-    
+    whole_background = np.concatenate(x_background)
+    whole_selected = []
     for idx in guide_idx:
         sgrna = specific_guides[idx]
         sg_x_background = x_background[idx]
         sg_y = y[idx]
         sg_otss = otss_dict[sgrna]
         sg_x_selected, sgrna_otss, additional_features = filter_data_for_interpertation(sg_x_background, sg_y, sg_otss,  only_seq, specific_indices)
-        temp_output = create_folder(output_path,sgrna)   
-        shap_values = get_shaply_values(model, sg_x_background, explainer_type, sg_x_selected)
-        plot_shap(shap_values, additional_features, sgrna_otss, temp_output)
+        whole_selected.append(sg_x_selected)
+        # NOTE: Background set is spesific to each gRNA, maybe check for equal background for all guides.
+        if plot_single_guides:
+            temp_output = create_folder(output_path,sgrna)
+            shap_values = get_shaply_values(model, whole_background, explainer_type, sg_x_selected)
+            plot_shap(shap_values, additional_features, sgrna_otss, temp_output)
+    # plot all guides
+    if plot_all_guides:
+        whole_selected = [x[:100] for x in whole_selected] # get first 100 samples
+        whole_selected = np.concatenate(whole_selected)
+        shap_values = get_shaply_values(model, whole_background, explainer_type, whole_selected)
+        temp_output = create_folder(output_path,"All_guides")
+        plot_shap(shap_values, additional_features, None, temp_output)
+
 
 
 def plot_shap(shap_values, additional_features, sgrna_otss, output_path):
@@ -317,20 +331,20 @@ def plot_shap(shap_values, additional_features, sgrna_otss, output_path):
     sequence_shap_values, epigenetic_shap_values, min_shap,max_shap  = transform_to_heatmap(single_shap_values, 24,25,additional_features)
     kwargs = {'vmin': min_shap, 'vmax': max_shap,'cbar': 'SHAP values'}
     plot_subplots(sequence_shap_values,plot_types='heatmap',titles=None, x_label="Position", y_label="Nucleotides",x_ticks=x_ticks,
-                    y_ticks=row_labels, output_path=output_path, general_title="10-SHAP values",sgrna_otss=sgrna_otss,**kwargs)
+                    y_ticks=row_labels, output_path=output_path, general_title="10-SHAP values all_bg",sgrna_otss=sgrna_otss,**kwargs)
     # Summarized plot of all samples
     summarized_shap_values = np.sum(shap_values.values, axis=0)
     sequence_shap_values, epigenetic_shap_values, min_shap,max_shap  = transform_to_heatmap(summarized_shap_values, 24,25,additional_features)
     kwargs = {'vmin': min_shap, 'vmax': max_shap,'cbar': 'SHAP values'}
 
     plot_subplots(sequence_shap_values,plot_types='heatmap',titles=None, x_label="Position", y_label="Nucleotides",x_ticks=x_ticks,
-                    y_ticks=row_labels, output_path=output_path, general_title="Summed-SHAP values",sgrna_otss=None,**kwargs)
+                    y_ticks=row_labels, output_path=output_path, general_title="Summed-SHAP values all_bg",sgrna_otss=None,**kwargs)
     # Abs mean
     abs_mean_shap_values = np.mean(np.abs(shap_values.values), axis=0)
     sequence_shap_values, epigenetic_shap_values, min_shap,max_shap  = transform_to_heatmap(abs_mean_shap_values, 24,25,additional_features)
     kwargs = {'vmin': min_shap, 'vmax': max_shap,'cbar': 'SHAP values'}
     plot_subplots(sequence_shap_values,plot_types='heatmap',titles=None, x_label="Position", y_label="Nucleotides",x_ticks=x_ticks,
-                        y_ticks=row_labels, output_path=output_path, general_title="AbsMean-SHAP values",sgrna_otss=None,**kwargs)
+                        y_ticks=row_labels, output_path=output_path, general_title="AbsMean-SHAP values all_bg",sgrna_otss=None,**kwargs)
 
 def main_shap():
     epi_model_path = "/localdata/alon/Models/Change-seq/vivo-silico/Exclude_Refined_TrueOT/Classification/No_constraints/Full_encoding/No_CW/GRU-EMB/5epochs_1024_batch/Early_stop/Ensemble/With_features_by_columns/All_guides/1_ensembels/50_models/Binary_epigenetics/All-epigenetics/ensemble_1/model_1.keras"
@@ -342,7 +356,7 @@ def main_shap():
     specific_guides = None
     number_of_points = 10
     run_shap(model_path=seq_model_path,data_path=test_data_path,explainer_type=explainer_type,
-             output_path=output_path,num_of_points=number_of_points,specific_guides=specific_guides,only_seq=True)
+             output_path=output_path,num_of_points=number_of_points,specific_guides=specific_guides,only_seq=True,plot_all_guides=False)
 
 ##################### Gradient asecnt #####################
 def main_gradient_ascent():
@@ -467,8 +481,50 @@ def get_gradients(model, input_data):
 
 #     return titles 
     
-    
-    
+##################### Epigenetics #####################   
+def main_epigenetics():
+    epi_model_path = "/localdata/alon/Models/Change-seq/vivo-silico/Exclude_Refined_TrueOT/Classification/No_constraints/Full_encoding/No_CW/GRU-EMB/5epochs_1024_batch/Early_stop/Ensemble/With_features_by_columns/All_guides/1_ensembels/50_models/Binary_epigenetics/All-epigenetics/ensemble_1/model_1.keras"
+    test_data_path = "/home/dsi/lubosha/Off-Target-data-proccessing/Data/TrueOT/Refined_TrueOT_Lazzarotto_withEpigenetic.csv"
+    output_path = '/home/dsi/lubosha/Off-Target-data-proccessing/Plots/Change-seq/vivo-silico/Exclude_Refined_TrueOT/on_Refined_TrueOT_Lazzarroto/Classification/No_constraints/Full_encoding/No_CW/GRU-EMB/5epochs_1024_batch/Early_stop/Ensemble/Model_interpertability'
+    specific_guides = None
+    number_of_points = 10
+    run_epigenetics(model_path=epi_model_path,data_path=test_data_path,output_path=output_path,
+                    num_of_points=number_of_points,specific_guides=specific_guides)
+def run_epigenetics(model_path ,data_path , output_path = None, num_of_points = 200,
+                     specific_guides = None, by = None, features = None):
+    '''
+    This function will run epigenetic interpertation on the given model and data.
+    It will interpert eather by petrubating the epigenetic features or by constant values (0.5) for each feature not evaluated.
+    Args:
+        model_path (str): path to the model/folder of models
+        data_path (str): path to the data
+        output_path (str): path to save the plots
+        num_of_points (int, optional): Number of points to interpert - default 200
+            None: Balanced amount of positive and negatives will be returned.
+            0: all positives will be returned.
+            >0: total number of points to sample.
+        specific_guides (list, optional): Specific guides to extract from the data.
+        by (str, optional): If 'pertubation' the epigenetic features will be pertubated, otherwise constant values will be used.
+    '''
+    models = get_model(model_path,"deep")
+    model = models[0]
+    # x_background,y,guides,otss_dict = get_data(data_path,only_seq=True)
+    # if specific_guides is None:
+    #     specific_guides = guides
+    # guide_idx = keep_intersect_guides_indices(guides,specific_guides)
+    # # get one sample
+    # sg_x, y_x, sg_ots = x_background[guide_idx[0]], y[guide_idx[0]], guides[guide_idx[0]]
+    # ots_1_vector, ots_1_seq = sg_x[0], otss_dict[sg_ots][0]
+    # get epigenetic features
+    sg_x = np.random.randint(2, size=(1, 600), dtype=np.int8)
+    features = ["H3K27me3_peaks_binary", "H3K27ac_peaks_binary", "H3K9ac_peaks_binary", "H3K9me3_peaks_binary", "H3K36me3_peaks_binary", "ATAC-seq_peaks_binary", "H3K4me3_peaks_binary", "H3K4me1_peaks_binary"]
+    pert_import = epigenetic_pertubation_importance(features,sg_x, model)
+    importance_05 = epigenetic_05_importance(features,sg_x, model)
+    pert_import = {key: np.mean(value) for key, value in pert_import.items()}
+    print(pert_import)
+    print(importance_05)
+
+     
 if __name__ == "__main__":
-    main_gradient_ascent()
+    main_epigenetics()
     

interpertation_utilities.py
@@ -3,7 +3,10 @@ This module contain helper function and utilities for model and data interpertab
 '''
 import numpy as np
 from features_engineering import extract_features, generate_features_and_labels
+from features_and_model_utilities import get_feature_name
+
 from file_utilities import create_paths
+import itertools
 
 
 ######## DATA ########
@@ -13,7 +16,6 @@ def nucleotides_for_heatmap():
     Returns:
         row_labels (list): List of row labels.
         x_ticks (list): List of x-ticks.'''
-    import itertools
     nucleotides_product = list(itertools.product(*(["ACGT-"] * 2)))
     row_labels = [f"{a}:{b}" for a, b in nucleotides_product]
     x_ticks = list(range(1, 25))
@@ -164,6 +166,109 @@ def convert_features_names(shap_values, agg_function, seqeunce_length, bits_per_
     shap_values.values = grouped_shap_values
     shap_values.feature_names = feature_names
     shap_values.data = shap_values.data[:, [g[0] for g in groups]]
-    
-    
     return shap_values
+
+######## Epigenetics ########
+def epigenetic_05_importance(features, sg_ot_pair, model):
+    '''
+    Function calculate the epigentic feature importance by assigining 0.5 to the epigenetic features.
+    It substracts the prediction of the model where the wanted epigenetic feature is 1/0.
+    If the Delta is positive the feature is important.
+    Args:
+        features (list): List of epigenetic features.
+        sg_ot_pair (np.array): Pair of sgRNA and off-target.
+        model (tf.keras.Model): Model to interpret.
+    Returns:
+        dict: Dictionary of epigenetic feature importance.
+    '''
+    epi_feature_importance = {}
+    constant_sg_ot = np.repeat(sg_ot_pair, 2, axis=0) # Repeat the same sgRNA-OT pair
+    for feature_index, feature in enumerate(features): # Iterate over features
+        feature_name = get_feature_name(feature)
+        epi_vector = np.array(2*[[0.5]*len(features)]) # Create epigenetic vector with 0.5
+        epi_vector[0, feature_index] = 0 # Set the wanted feature to 0
+        epi_vector[1, feature_index] = 1 # Set the wanted feature to 1
+        x_input = [constant_sg_ot, epi_vector] # Create input for model
+        model_output = model.predict(x_input) # Get model output
+        epi_feature_importance[feature_name] = model_output[1][0] - model_output[0][0] # Calculate difference
+    return epi_feature_importance
+
+def epigenetic_pertubation_importance(features, sg_ot_pair, model):
+    '''
+    Function calculate the epigentic feature importance by perturbarting over the epigenetic features.
+    It substracts the prediction of the model where the epigenetic feature is on and off.
+    If the Delta is positive the feature is important.
+
+    Creates 2^(number_of_features - 1) epigenetic vectors pairs.
+    Each pair has the same indexes on except for the given index.
+    For example: number_of_features = 3, index = 0 -> create pairs of (0,1,1) - (1,1,1), (0,0,1) - (1,0,1) ...
+    So the wanted epigenetic feature is off/on in the pairs.
+    Args:
+        features (list): List of epigenetic features.
+        sg_ot_pair (np.array): Pair of sgRNA and off-target.
+        model (tf.keras.Model): Model to interpret.
+    Returns:
+        dict: Dictionary of epigenetic feature importance.
+    '''
+    all_combination_epi_vectors = generate_all_bit_combinations(len(features))
+    constant_x = np.repeat(sg_ot_pair, len(all_combination_epi_vectors), axis=0) # Repeat the same sgRNA-OT pair
+    x_input = [constant_x, all_combination_epi_vectors] # Create input for model
+    model_output = model.predict(x_input) # Get model output
+    bit_values = {tuple(bits): prediction[0] for bits, prediction in zip(all_combination_epi_vectors, model_output)} # Create dict of bit values
+    epi_feature_importance = {}
+    for feature_index, feature in enumerate(features): # Iterate over features
+        feature_name = get_feature_name(feature)
+        epi_feature_importance[feature_name] = subtract_pairs_by_masking(bit_values, feature_index,len(features))
+    return epi_feature_importance
+
+def generate_all_bit_combinations(N):
+    '''
+    Generate all possible bit combinations for N bits.
+    Args:
+        N (int): Number of bits.
+    Returns:
+        np.array: All possible bit combinations'''
+    return np.array(list(itertools.product([0, 1], repeat=N)))
+
+def subtract_pairs_by_masking(bit_values, i,N):
+    '''
+    Substract matching values of pairs of vectors where the i-th bit is on and off.
+    Args:
+        bit_values (dict): Dictionary of vectors and their values.
+        i (int): Index of the bit to mask.
+    Returns:
+        np.array: Differences between matching pairs.
+    '''
+    bit_combinations = np.array(list(bit_values.keys()))  # Convert dict keys to array
+    values = np.array(list(bit_values.values()))  # Convert dict values to array
+
+    # Create masks
+    mask_on = bit_combinations[:, i] == 1  # Select where bit i is 1
+    mask_off = bit_combinations[:, i] == 0  # Select where bit i is 0
+
+    bits_on = bit_combinations[mask_on]  # Entries where bit i is 1
+    bits_off = bit_combinations[mask_off]  # Entries where bit i is 0
+    
+    values_on = values[mask_on]  # Values for bit i = 1
+    values_off = values[mask_off]  # Values for bit i = 0
+    
+    bits_on_reduced = np.delete(bits_on, i, axis=1)
+    bits_off_reduced = np.delete(bits_off, i, axis=1)
+
+    # Convert to structured arrays for row-wise matching
+    dtype = [('f{}'.format(j), bits_on_reduced.dtype) for j in range(N-1)]
+    bits_on_struct = bits_on_reduced.view(dtype)
+    bits_off_struct = bits_off_reduced.view(dtype)
+
+    # Find matching pairs
+    _, idx_on, idx_off = np.intersect1d(bits_on_struct, bits_off_struct, return_indices=True)
+
+    # Compute differences for matched indices
+    differences_ = values_on[idx_on] - values_off[idx_off]
+    differences = values_on - values_off  # Calculate differences
+    print(np.equal(differences, differences_).all())  # Check if differences are equal
+    
+    return differences  
+
+
+    

models.py
@@ -3,7 +3,7 @@
 
 from xgboost import XGBClassifier
 from sklearn.linear_model import LogisticRegression
-
+import tensorflow as tf
 from tensorflow import keras,argmax
 from keras.layers import Reshape, Conv1D, Input, Dense, Flatten, Concatenate, MaxPooling1D, Reshape, Embedding, GRU
 
@@ -173,4 +173,44 @@ def task_model_parameters(task):
         return keras.losses.MeanSquaredError(), ['mean_absolute_error'], 'linear'
     else:
         raise ValueError("Task must be set to 'Classification' or 'Regression'")
+
+
+def argmax_with_ste(x):
+    indices = tf.argmax(x, axis=-1)
+    one_hot = tf.one_hot(indices, depth=tf.shape(x)[-1])
+    return x + tf.stop_gradient(one_hot - x)  # STE approximation    
+
+def replace_argmax_layer(old_model, replacement_function = argmax_with_ste):
+    sequence_length, vocab_size = 24,25
+    inputs = Input(shape=(24, 25))
     
+    # Replace argmax layer
+    reduced_input = keras.layers.Lambda(argmax_with_ste)(inputs)
+    
+    # Rebuild remaining layers
+    embedding_layer = Embedding(input_dim=vocab_size, output_dim=44, input_length=sequence_length)
+    embedded_output = embedding_layer(reduced_input)
+    
+    gru = GRU(64, return_sequences=True)
+    gru_output = gru(embedded_output)
+    
+    x = keras.layers.Flatten()(gru_output)
+    
+    if hasattr(old_model, 'feature_input'):
+        feature_input = Input(shape=(old_model.feature_input.shape[1],))
+        x = Concatenate()([x, feature_input])
+        inputs = [inputs, feature_input]
+    
+    for dense_layer in old_model.layers[3:]:
+        x = Dense(dense_layer.units, activation=dense_layer.activation)(x)
+    
+    output = Dense(1, activation=old_model.layers[-1].activation)(x)
+    
+    new_model = keras.Model(inputs=inputs, outputs=output)
+    
+    # Copy weights from old model, except for replaced layers
+    for old_layer, new_layer in zip(old_model.layers, new_model.layers):
+        if not isinstance(old_layer, keras.layers.Lambda):
+            new_layer.set_weights(old_layer.get_weights())
+    
+    return new_model